18e14e75a922e0a5b945407577ce0a5127983b0a
Prathamesh-1701 <85896931+Prathamesh-1701@users.noreply.github.com>
2024-01-21 23:41:38 +0530

    Update and rename Sidebar.tsx to Sidebar.jsx

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -9,6 +9,7 @@ import {
   Drawer,
   DrawerContent,
   useDisclosure,
+  ChakraProvider,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { MdAccountBox } from 'react-icons/md';
@@ -22,15 +23,7 @@ import {
 import { IconType } from 'react-icons';
 import { ReactText } from 'react';
 
-interface LinkItemProps {
-  name: string;
-  icon: IconType;
-  link: string;
-}
-
-
-
-const LinkItems: Array<LinkItemProps> = [
+const LinkItems = [
   { name: 'Home', icon: FiHome, link: '/' },
   { name: 'Forcast', icon: FiTrendingUp, link: '/forecast' },
   { name: 'Shop', icon: FaShoppingCart, link: '/shop' },
@@ -40,26 +33,20 @@ const LinkItems: Array<LinkItemProps> = [
 ];
 
 const bgStyle = {
-  // bgGradient='linear(to-r, green.200, pink.500)'
-  // backgroundColor: '#61554D',
   backdropFilter: 'blur(0.01px)',
   backgroundImage: `url(https://plus.unsplash.com/premium_photo-1678344165196-261064eb31e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2187&q=80)`,
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
-  // opacity: .7,
 };
 
 const bgStyle1 = {
-  // bgGradient='linear(to-r, green.200, pink.500)'
-  // backgroundColor: '#61554D',
   backdropFilter: 'blur(0.01px)',
   backgroundImage: `url(https://img.freepik.com/free-photo/brown-soil-background-gardening_53876-133209.jpg?w=1060&t=st=1696231750~exp=1696232350~hmac=3aed6304a09c1a38c427688b05324b197990260aab1ae256397952796cede031)`,
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
 };
 
-
-const SidebarContent = ({ onClose }: { onClose: () => void }) => {
+const SidebarContent = ({ onClose }) => {
   return (
     <Box
       style={bgStyle}
@@ -71,22 +58,20 @@ const SidebarContent = ({ onClose }: { onClose: () => void }) => {
       bgGradient="linear(to-r, #61554D, #3F312D)"
       w={{ base: '80%', md: '300px' }}
       overflowY="auto"
-      // zIndex={10}
     >
       <Flex h="200px" alignItems="center" mx="16" justifyContent="space-between">
-        <Box  display={'flex'} flexDirection={'row'} width={'full'}>
-        <Text
-          bgGradient="linear(to-t, #B2B4B4, #FFFFFF, #FFFFFF)"
-          bgClip="text"
-          // marginRight="29%"
-          fontSize="2xl"
-          fontWeight="bold"
-        >
-          Empowering Sustainable Agriculture Website
-        </Text>
-        <Box bg={'white'} height={'2%'} marginTop={'89%'} borderRadius={'8%'}>
-        <CloseButton  display={{ base: 'flex', md: 'flex' }} onClick={onClose}  />
-        </Box>
+        <Box display={'flex'} flexDirection={'row'} width={'full'}>
+          <Text
+            bgGradient="linear(to-t, #B2B4B4, #FFFFFF, #FFFFFF)"
+            bgClip="text"
+            fontSize="2xl"
+            fontWeight="bold"
+          >
+            Empowering Sustainable Agriculture Website
+          </Text>
+          <Box bg={'white'} height={'2%'} marginTop={'89%'} borderRadius={'8%'}>
+            <CloseButton display={{ base: 'flex', md: 'flex' }} onClick={onClose} />
+          </Box>
         </Box>
       </Flex>
       {LinkItems.map((link) => (
@@ -98,13 +83,7 @@ const SidebarContent = ({ onClose }: { onClose: () => void }) => {
   );
 };
 
-interface NavItemProps {
-  icon: IconType;
-  children: ReactText;
-  link: string;
-}
-
-const NavItem = ({ icon, children, link }: NavItemProps) => {
+const NavItem = ({ icon, children, link }) => {
   return (
     <Link to={link}>
       <Box
@@ -128,55 +107,44 @@ const NavItem = ({ icon, children, link }: NavItemProps) => {
   );
 };
 
-const MobileNav = ({ onOpen }: { onOpen: () => void }) => {
+const MobileNav = ({ onOpen }) => {
   return (
-    <Box
-      position="fixed"
-      top={0}
-      left={0}
-      height="100%"
-      width="full"
-      color="white"
-      overflow="hidden"
-      zIndex={10}
-    >
-      <Flex
-        style={bgStyle1}
-        bgGradient="linear(to-t, #61554D, #3F312D)"
-        width="full"
-        marginBottom="2%"
-        justifyContent="space-between"
-      >
-        <Box display="flex">
-          <IconButton
-            bg="white"
-            margin="3vh"
-            variant="outline"
-            onClick={onOpen}
-            aria-label="open menu"
-            icon={<FiMenu />}
-          />
-        </Box>
-
-        <Box  bg="transparent" ml="auto" mr="auto">
-          
-        
-        <Text
-          bgGradient="linear(to-b, #B2B4B4, #FFFFFF, #52AE22)"
-          bgClip="text"
-          fontSize="5xl"
-          fontWeight="extrabold"
-          fontStyle="oblique"
-          fontFamily="monospace"
-          // paddingRight="23%"
-          justifyContent={'center'}
-          alignItems={'center'}
+    <ChakraProvider>
+      <Box margin={'2.5%'} height="100%" width="full" color="white" overflow="hidden" zIndex={10}>
+        <Flex
+          style={bgStyle1}
+          bgGradient="linear(to-t, #61554D, #3F312D)"
+          width="full"
+          justifyContent="space-between"
         >
-          Harvest Heaven
-        </Text>
-        </Box>
-      </Flex>
-    </Box>
+          <Box display="flex">
+            <IconButton
+              bg="white"
+              margin="3vh"
+              variant="outline"
+              onClick={onOpen}
+              aria-label="open menu"
+              icon={<FiMenu />}
+            />
+          </Box>
+
+          <Box ml="auto" mr="auto">
+            <Text
+              bgGradient="linear(to-b, #B2B4B4, #FFFFFF, #52AE22)"
+              bgClip="text"
+              fontSize="5xl"
+              fontWeight="extrabold"
+              fontStyle="oblique"
+              fontFamily="monospace"
+              justifyContent={'center'}
+              alignItems={'center'}
+            >
+              Harvest Heaven
+            </Text>
+          </Box>
+        </Flex>
+      </Box>
+    </ChakraProvider>
   );
 };
 
@@ -186,21 +154,18 @@ const Sidebar = () => {
   return (
     <Box display="flex">
       <Drawer
-
-        
         isOpen={isOpen}
         placement="left"
         onClose={onClose}
         returnFocusOnClose={false}
         onOverlayClick={onClose}
-        
       >
         <DrawerContent bg={'transparent'} boxShadow={'none'}>
           <SidebarContent onClose={onClose} />
         </DrawerContent>
       </Drawer>
       <MobileNav onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 0 }} width={{ base: '0%', md: '20%' }} />
+      <Box ml={{ base: 0, md: 0 }} width={{ base: '0%', md: 'auto' }} />
     </Box>
   );
 };
